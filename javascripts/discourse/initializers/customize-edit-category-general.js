66bf74f0b1d03713224ce8e43ee68ad28dfc0e45
Nazarii <devdragon1129@gmail.com>
2023-01-12 18:44:14 +0800

    get current hostname 9

--- javascripts/discourse/initializers/customize-edit-category-general.js
+++ javascripts/discourse/initializers/customize-edit-category-general.js
@@ -6,13 +6,6 @@ export default {
 
   initialize() {
     withPluginApi("0.8.14", (api) => {
-      const org_path = window.location.hostname;
-      window.alert("Start getting org_path!");
-      if(!org_path) 
-        window.alert("No org_path!");
-      else
-        window.alert(org_path);
-
       api.modifyClass("component:edit-category-general", {
         pluginId: "discourse-air",
 
