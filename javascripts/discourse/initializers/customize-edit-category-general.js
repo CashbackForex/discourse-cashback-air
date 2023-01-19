6a66872f2a9ff08c8f69202587f59c375d803140
Nazarii <devdragon1129@gmail.com>
2023-01-19 17:28:06 +0800

    plus for link backs

--- javascripts/discourse/initializers/customize-edit-category-general.js
+++ javascripts/discourse/initializers/customize-edit-category-general.js
@@ -1,5 +1,8 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { cancel } from "@ember/runloop";
+import { h } from "virtual-dom";
+import { wantsNewWindow } from "discourse/lib/intercept-click";
+import DiscourseURL from "discourse/lib/url";
 
 export default {
   name: "customize-edit-category-general",
@@ -21,6 +24,94 @@ export default {
           this._laterFocus && cancel(this._laterFocus);
         },
       });
+
+      const domain = window.location.hostname;
+      window.alert(domain);
+      let goBackDomainLinks = '';
+      if(domain == 'talktrading.io') return;
+      else if (domain == 'cbf.talktrading.io') goBackDomainLinks = settings.goBack_cbf_links;
+      else if (domain == 'rkfx.talktrading.io') goBackDomainLinks = settings.goBack_rkfx_links;
+      
+      if (!goBackDomainLinks.length) {
+        return;
+      }
+
+      const linksPosition =
+        settings.links_position === "right"
+          ? "header-buttons:before"
+          : "home-logo:after";
+
+      const headerLinks = [];
+
+      goBackDomainLinks
+        .split("|")
+        .filter(Boolean)
+        .map((goBackDomainLinksArray) => {
+          const [linkText, linkTitle, linkHref, device, target, keepOnScroll] =
+            goBackDomainLinksArray
+              .split(",")
+              .filter(Boolean)
+              .map((x) => x.trim());
+
+          const deviceClass = `.${device}`;
+          const linkTarget = target === "self" ? "" : "_blank";
+          const keepOnScrollClass = keepOnScroll === "keep" ? ".keep" : "";
+          const linkClass = `.${linkText
+            .toLowerCase()
+            .replace(/\s/gi, "-")}-custom-header-links`;
+
+          const anchorAttributes = {
+            title: linkTitle,
+            href: linkHref,
+          };
+          if (linkTarget) {
+            anchorAttributes.target = linkTarget;
+          }
+
+          headerLinks.push(
+            h(
+              `li.headerLink${deviceClass}${keepOnScrollClass}${linkClass}`,
+              h("a", anchorAttributes, linkText)
+            )
+          );
+        });
+
+      api.decorateWidget(linksPosition, (helper) => {
+        return helper.h("ul.custom-header-links", headerLinks);
+      });
+
+      api.decorateWidget("home-logo:after", (helper) => {
+        const dHeader = document.querySelector(".d-header");
+
+        if (!dHeader) {
+          return;
+        }
+
+        const isTitleVisible = helper.attrs.minimized;
+        if (isTitleVisible) {
+          dHeader.classList.add("hide-menus");
+        } else {
+          dHeader.classList.remove("hide-menus");
+        }
+      });
+
+      if (settings.links_position === "left") {
+        // if links are aligned left, we need to be able to open in a new tab
+        api.reopenWidget("home-logo", {
+          click(e) {
+            if (e.target.id === "site-logo") {
+              if (wantsNewWindow(e)) {
+                return false;
+              }
+              e.preventDefault();
+
+              DiscourseURL.routeToTag($(e.target).closest("a")[0]);
+
+              return false;
+            }
+          },
+        });
+      }
     });
   },
 };
