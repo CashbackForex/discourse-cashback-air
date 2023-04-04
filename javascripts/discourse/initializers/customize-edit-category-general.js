23198d023fa3db7b3b3df72fe1a0ab2a66ffd84e
Nazarii <devdragon1129@gmail.com>
2023-04-05 04:27:54 +0800

    check some 2

--- javascripts/discourse/initializers/customize-edit-category-general.js
+++ javascripts/discourse/initializers/customize-edit-category-general.js
@@ -32,8 +32,9 @@ export default {
       
       let goBackDomainLinks = '';
       
-      if (window.portal == 'CBF') goBackDomainLinks = settings.goBack_cbf_links;
-      else if (window.portal == 'RKFX') goBackDomainLinks = settings.goBack_rkfx_links;
+      if (domain.includes('cbf')) goBackDomainLinks = settings.goBack_cbf_links;
+      else if (domain.includes('rkfx')) goBackDomainLinks = settings.goBack_rkfx_links;
+      else return;
       
       if (!goBackDomainLinks.length) {
         return;
